9dabb3c5b5575c2ce5a66e9ecf4de25f7bd926c6
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-08-15 14:28:52 +0500

    Add kitten icat alias

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -21,7 +21,7 @@ alias pamcan pacman
 alias ls 'eza --icons'
 alias clear "printf '\033[2J\033[3J\033[1;1H'"
 alias q 'qs -c ii'
-    
+alias icat 'kitten icat'
 
 # function fish_prompt
 #   set_color cyan; echo (pwd)
